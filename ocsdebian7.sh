4d5270d447701e06d431f02abcbfdefdd5eb5bdf
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2018-03-13 22:13:09 +0700

    Update ocsdebian7.sh

--- ocsdebian7.sh
+++ ocsdebian7.sh
@@ -12,8 +12,8 @@ apt-get -y install nginx php5 php5-fpm php5-cli php5-mysql php5-mcrypt
 rm /etc/nginx/sites-enabled/default && rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
 mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup
-wget -O /etc/nginx/nginx.conf "https://raw.github.com/blazevpn/ocspanel/master/nginx.conf"
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/blazevpn/ocspanel/master/vps.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.github.com/thirdza056/ocspanel/master/nginx.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.github.com/thirdza056/ocspanel/master/vps.conf"
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
@@ -39,7 +39,7 @@ apt-get -y install git
 #install OCS Panel
 cd /home/vps/public_html
 git init
-git remote add origin https://github.com/stevenindarto/OCSPanel.git
+git remote add origin https://github.com/thirdza056/ocspanel.git
 git pull origin master
 chmod 777 /home/vps/public_html/config
 chmod 777 /home/vps/public_html/config/config.ini
